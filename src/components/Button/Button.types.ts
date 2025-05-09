67e956538f30678578b4fcb13eac2d9a1499a81a
BigDaveCoding <bigdavecoding@gmail.com>
2025-05-09 23:49:44 +0100

    export custom types and interface for button properties

---
+++ src/components/Button/Button.types.ts
@@ -0,0 +1,13 @@
+import React from "react";
+
+// creating custom typescript types
+// variant and size must be one of the options provided
+export type ButtonVariant = 'primitive' | 'simple' | 'rounded' | 'outline' | 'bordered';
+export type ButtonSize = 'small' | 'medium' | 'large';
+
+// ButtonHTMLAttributes inherits all standard button attributes i.e. onClick | disabled etc.
+export interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+    children: React.ReactNode; // content inside component has to be valid react content
+    variant?: ButtonVariant; // optional props - if not provided fallback to default value - optional values set in component
+    size?: ButtonSize;
+}
