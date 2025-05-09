885931bc41711cf9692fcf3071d5ae99a3a92ac2
BigDaveCoding <bigdavecoding@gmail.com>
2025-05-09 23:50:31 +0100

    variants for different button types - need to add the rest in - started with 5

---
+++ src/components/Button/Button.variants.ts
@@ -0,0 +1,27 @@
+export const buttonVariants = {
+    'primitive' : {
+        'default' : 'bg-gray6 text-white',
+        'hover' : 'hover:bg-gray7'
+    },
+    'simple' : {
+        'default' : 'bg-gray6 text-white rounded-md',
+        'hover' : 'hover:bg-gray7'
+    },
+    'rounded' : {
+        'default' : 'bg-gray6 text-white rounded-full',
+        'hover' : 'hover:bg-gray7'
+    },
+    'outline' : {
+        'default' : 'bg-white border rounded-md ',
+        'hover' : 'hover:bg-gray7 hover:text-white'
+    },
+    'bordered' : {
+        'default' : 'bg-gray6 border border-gray7 rounded-md text-white',
+        'hover' : 'hover:bg-gray7'
+    },
+    'sizes' : {
+        'small' : 'text-sm px-2 py-1',
+        'medium' : 'px-4 py-2',
+        'large' : 'text-lg px-6 py-3',
+    }
+}
