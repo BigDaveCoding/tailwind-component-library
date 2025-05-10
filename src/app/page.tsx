99a9ea766d07f5205bf33daef06b678c014a3618
BigDaveCoding <bigdavecoding@gmail.com>
2025-05-10 11:21:41 +0100

    edit import filepath

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import { Button } from "@/components/Button";
-import ButtonGroup from "@/components/ButtonGroup/ButtonGroup";
+import { ButtonGroup } from "@/components/ButtonGroup";
 
 export default function Home() {
   return (
