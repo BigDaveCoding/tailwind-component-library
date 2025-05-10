28f5b4b7f2fcff24df3a21c3beb4cb786f3b3090
BigDaveCoding <bigdavecoding@gmail.com>
2025-05-10 10:44:16 +0100

    testing buttons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,24 +5,29 @@ export default function Home() {
   return (
     <>
         <p>hi</p>
-        <Button variant="primitive">
-            Button
-        </Button>
-        <Button variant={"simple"} >
-            Button
-        </Button>
-        <Button variant={"rounded"} >
-            Button
-        </Button>
-        <Button variant={"outline"} >
-            Button
-        </Button>
-        <Button variant={"bordered"} >
-            Button
-        </Button>
-        <Button variant={"3d"} >
-            Button
-        </Button>
+        <div className={"flex gap-2 m-2"}>
+            <Button variant="primitive">
+                Button
+            </Button>
+            <Button variant={"simple"} >
+                Button
+            </Button>
+            <Button variant={"rounded"} >
+                Button
+            </Button>
+            <Button variant={"outline"} >
+                Button
+            </Button>
+            <Button variant={"bordered"} >
+                Button
+            </Button>
+            <Button variant={"3d"} >
+                Button
+            </Button>
+            <Button variant={"elevated"} >
+                Button
+            </Button>
+        </div>
     </>
   );
 }
