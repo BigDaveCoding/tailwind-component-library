65edd2e8169017e81172cc5f6b0d3c9826f721ca
BigDaveCoding <bigdavecoding@gmail.com>
2025-05-10 22:52:43 +0100

    working on navigation component

---
+++ src/components/Navigation/NavBar.types.ts
@@ -0,0 +1,14 @@
+export type NavBarVariant = "default" | "collapsed" | "expanded";
+
+interface NavLinks {
+    label: string;
+    href: string;
+}
+
+export interface NavBarProps {
+    children?: React.ReactNode;
+    title?: string;
+    variant?: NavBarVariant;
+    links: NavLinks[];
+    buttonText?: string;
+}
---
+++ src/components/Navigation/NavBar.variants.ts
@@ -0,0 +1,23 @@
+export const NavBarVariants = {
+    'default' : {
+        'navContainer' : 'bg-green5 w-[850px] h-[72px] ',
+        'logoText' : 'text-white',
+        'logo' : 'size-10 text-white',
+        'links' : '',
+        'button' : '',
+    },
+    'collapsed' : {
+        'navContainer' : 'bg-gray4',
+        'logoText' : '',
+        'logo' : 'size-6 text-white',
+        'links' : '',
+        'button' : '',
+    },
+    'expanded' : {
+        'navContainer' : 'bg-gray4',
+        'logoText' : '',
+        'logo' : '',
+        'links' : '',
+        'button' : '',
+    }
+}
