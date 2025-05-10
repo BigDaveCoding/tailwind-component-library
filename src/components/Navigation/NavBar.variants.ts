3dc35c91299e7b840d44dfd22026804da106c568
BigDaveCoding <bigdavecoding@gmail.com>
2025-05-11 00:05:00 +0100

    working on styling of different variants and moving styling into variant file

--- src/components/Navigation/NavBar.variants.ts
+++ src/components/Navigation/NavBar.variants.ts
@@ -1,23 +1,26 @@
 export const NavBarVariants = {
     'default' : {
-        'navContainer' : 'bg-green5 w-[850px] h-[72px] ',
+        'navContainer' : 'bg-green5 max-w-[850px] h-[72px] ',
         'title' : 'text-white text-xl font-bold',
         'logo' : 'w-[32px] h-[19.2] text-white',
-        'links' : '',
+        'linkContainer' : 'px-6 gap-6',
+        'linkText' : '',
         'button' : '',
     },
     'collapsed' : {
         'navContainer' : 'bg-gray4',
         'title' : '',
         'logo' : 'w-[32px] h-[19.2] text-white',
-        'links' : '',
+        'linkContainer' : '',
+        'linkText' : '',
         'button' : '',
     },
     'expanded' : {
         'navContainer' : 'bg-gray4',
         'title' : '',
         'logo' : '',
-        'links' : '',
+        'linkContainer' : '',
+        'linkText' : '',
         'button' : '',
     }
 }
