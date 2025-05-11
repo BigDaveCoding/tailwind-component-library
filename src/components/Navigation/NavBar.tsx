3b1c53e4e631450e44d7722df3cfc6792aff3cbc
BigDaveCoding <bigdavecoding@gmail.com>
2025-05-11 18:33:08 +0100

    added const for isDefault - adding accessibiilty aria labels etc.

--- src/components/Navigation/NavBar.tsx
+++ src/components/Navigation/NavBar.tsx
@@ -12,6 +12,8 @@ export default function NavBar({
    ...props
 }: NavBarProps) {
 
+    const isDefault = variant === "default";
+
     const baseNavContainerStyle: string = 'px-4 flex items-center text-center';
     const navContainerStyle = NavBarVariants[variant].navContainer;
     const logoStyle = NavBarVariants[variant].logo;
@@ -21,11 +23,10 @@ export default function NavBar({
 
     return (
         <>
-            <nav className={`${baseNavContainerStyle} ${navContainerStyle}`}>
-
+            <nav aria-label="navigation bar" className={`${baseNavContainerStyle} ${navContainerStyle}`}>
                 <div className={"flex items-center mr-auto"}>
                     <div className={`flex items-center text-center gap-2`}>
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5"
+                        <svg xlinkTitle="Logo" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5"
                              stroke="currentColor" className={`${logoStyle}`}>
                             <path strokeLinecap="round" strokeLinejoin="round"
                                   d="M4.26 10.147a60.438 60.438 0 0 0-.491 6.347A48.62 48.62 0 0 1 12 20.904a48.62 48.62 0 0 1 8.232-4.41 60.46 60.46 0 0 0-.491-6.347m-15.482 0a50.636 50.636 0 0 0-2.658-.813A59.906 59.906 0 0 1 12 3.493a59.903 59.903 0 0 1 10.399 5.84c-.896.248-1.783.52-2.658.814m-15.482 0A50.717 50.717 0 0 1 12 13.489a50.702 50.702 0 0 1 7.74-3.342M6.75 15a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5Zm0 0v-3.675A55.378 55.378 0 0 1 12 8.443m-7.007 11.55A5.981 5.981 0 0 0 6.75 15.75v-1.5"/>
@@ -33,7 +34,7 @@ export default function NavBar({
                         <h1 className={`${titleStyle}`}>{title}</h1>
                     </div>
 
-                    {variant === "default" &&
+                    {isDefault &&
                         <div className={`flex ${linkContainerStyle}`}>
                         {links.map((link, index) => {
                             return (
@@ -45,9 +46,9 @@ export default function NavBar({
                 </div>
                 <div className={"ml-auto"}>
                     <Button className={`${buttonStyle}`} variant={"outline"}>
-                        {variant === "default"
-                            ? "Download"
-                            : <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                        {isDefault ?
+                            "Download"
+                            : <svg aria-label="open menu icon" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
                                 <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
                             </svg>
                         }
